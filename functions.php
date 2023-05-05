b8b7dafbe50bd8c8e1fb7316e2a0a5c2b7eb3bc3
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-05-05 18:46:58 +0200

    feat: :sparkles: Adds: huston, we have a problem here

--- functions.php
+++ functions.php
@@ -1,23 +1,27 @@
 <?php
-$legth = $_GET['passLegth'];
-
-function password_generator($passLength){
-    $pass_alphaNum=[];
-    $pass_numbers=["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-    $pass_alphabet_big=["A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    $pass_alphabet_small=["a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    $pass_symbols=["!", "£", "$", "%", "&", "/", "(", ")", "=", "?", "+", "-", "@", "#", ".", ","];
+$passLength = $_GET['passLegth'];
+var_dump($passLength);
+/* function password_generator($passLength){
+    $pass_characters=["1", "2", "3", "4", "5", "6", "7", "8", "9", "0","A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "£", "$", "%", "&", "/", "(", ")", "=", "?", "+", "-", "@", "#", ".", ","];
     $user_generated_password= "";
 
-    for ($i=0; $i <= $passLegth; $i++) {
+    /* for ($i=0; $i < $passLegth; $i++) */
+    
+        /* $pass_alphaNum = array_merge($pass_numbers, $pass_alphabet_small, $pass_alphabet_big, $pass_symbols); */
+        /* ^^^ Erano array che avevo creato per il bonus, che non faccio */
+        /* $user_generated_password .= $pass_characters[random_int(0, strlen($pass_characters) - 1)]; */
+        /* ^^^ vecchio tentativo */
+    /*     $randomIndex = array_rand($pass_characters, $passLength);
+        var_dump($randomIndex); */
+    /* while (strlen($user_generated_password) < $passLength) {
         
-        $pass_alphaNum = array_merge($pass_numbers, $pass_alphabet_small, $pass_alphabet_big, $pass_symbols);
-        $alphaNum = rand(o, count($pass_alphaNum));
+        $user_generated_password .= $pass_characters[$randomIndex];
+        var_dump($user_generated_password);
+    }; */
+   /*  return $user_generated_password; */
 
-        $user_generated_password .= $pass_element[$element];
-    }
-    return $user_generated_password;
-}
+/* password_generator($passLength);  */
+
 ?>
 
 <!DOCTYPE html>
@@ -34,7 +38,7 @@ function password_generator($passLength){
    <p style="font-size: 2rem">
        La tua nuova password è: "
        <?php 
-           echo $user_generated_password;
+           
        ?>
        "
    </p>
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php include_once "/functions.php"; ?>
+<?php include "/functions.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Generate Psw PHP</title>
 </head>
 <body>
    <form action="functions.php" method="GET">
@@ -15,7 +15,6 @@
         <input id="passLength" type="text" name="passLength">
 
         <button type="submit">Genera</button>
-
    </form>
 </body>
 </html>
