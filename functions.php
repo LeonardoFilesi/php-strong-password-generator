b61426c07b3af6ce5174b9e2db5b8d3e066a39ec
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-05-05 19:08:49 +0200

    feat: :sparkles: Adds: completition milestone 1 & 2

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
-$passLength = $_GET['passLegth'];
+$passLength = $_GET['passLength'];
 var_dump($passLength);
 /* function password_generator($passLength){
     $pass_characters=["1", "2", "3", "4", "5", "6", "7", "8", "9", "0","A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "£", "$", "%", "&", "/", "(", ")", "=", "?", "+", "-", "@", "#", ".", ","];
     $user_generated_password= "";
 
-    /* for ($i=0; $i < $passLegth; $i++) */
+    for ($i=0; $i < $passLegth; $i++)
     
         /* $pass_alphaNum = array_merge($pass_numbers, $pass_alphabet_small, $pass_alphabet_big, $pass_symbols); */
         /* ^^^ Erano array che avevo creato per il bonus, che non faccio */
@@ -21,7 +21,20 @@ var_dump($passLength);
    /*  return $user_generated_password; */
 
 /* password_generator($passLength);  */
+    function password_generator($length){
+        $pass_characters=["1", "2", "3", "4", "5", "6", "7", "8", "9", "0","A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "£", "$", "%", "&", "/", "(", ")", "=", "?", "+", "-", "@", "#", ".", ","];
+        /* return $pass_characters; */
+        $user_generated_password= "";
+        $randomIndex = array_rand($pass_characters);
+        /* var_dump($pass_characters[$randomIndex]); */
 
+        while (strlen($user_generated_password) < $length){
+            $randomIndex = array_rand($pass_characters);
+            $user_generated_password .= $pass_characters[$randomIndex];
+        }
+        /* var_dump($user_generated_password); */
+        return $user_generated_password;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -38,7 +51,7 @@ var_dump($passLength);
    <p style="font-size: 2rem">
        La tua nuova password è: "
        <?php 
-           
+           echo password_generator($passLength);
        ?>
        "
    </p>
